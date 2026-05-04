18517dee081f03062aa46bd54eceb629a6107799
Rufio244 <thanvas9991@gmail.com>
2026-05-04 10:46:34 +0700

    Update rollback.py

--- crakauto/rollback.py
+++ crakauto/rollback.py
@@ -1,1 +1,10 @@
+import subprocess
 
+def update_system():
+    print("Updating system...")
+
+    subprocess.run(["git", "pull"])
+    subprocess.run(["docker-compose", "build"])
+    subprocess.run(["docker-compose", "up", "-d"])
+
+    print("Update complete")
