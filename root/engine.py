5a08e63b75a8400111b03bf7a8a69befb6dc2d5e
Rufio244 <thanvas9991@gmail.com>
2026-05-04 11:08:36 +0700

    Update engine.py

--- root/engine.py
+++ root/engine.py
@@ -1,1 +1,16 @@
+import math
 
+SAFE_FUNCS = {
+    "log": lambda x: math.log(max(x, 1e-9)),
+    "exp": math.exp,
+    "sqrt": math.sqrt,
+    "max": max,
+    "min": min,
+}
+
+def evaluate(expr: str, context: dict):
+    # จำกัด namespace เพื่อความปลอดภัย
+    env = {}
+    env.update(SAFE_FUNCS)
+    env.update(context)
+    return eval(expr, {"__builtins__": {}}, env)
