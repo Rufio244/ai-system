b2aaca716ade4a77f0dcbe7bbfcf553160dea8f8
Rufio244 <thanvas9991@gmail.com>
2026-05-04 11:09:04 +0700

    Update registry.py

--- root/registry.py
+++ root/registry.py
@@ -1,1 +1,12 @@
 
+FORMULAS = {
+    "default": "0.6*revenue + 0.3*ctr + 0.1*engagement - 0.2*latency",
+    "growth":  "0.4*revenue + 0.4*ctr + 0.2*new_users",
+    "profit":  "1.0*revenue - 0.5*cost"
+}
+
+def get_formula(name="default"):
+    return FORMULAS.get(name, FORMULAS["default"])
+
+def set_formula(name, expr):
+    FORMULAS[name] = expr
